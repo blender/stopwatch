25d0bfedb94e2c8208564b8a8e56b29728c797d7
Tommaso Piazza <tommaso.piazza@wearezeta.com>
2015-07-29 23:53:56 +0200

    Initial Commit

---
+++ stopwatch/stopwatch.h
@@ -0,0 +1,19 @@
+//
+//  stopwatch.h
+//  stopwatch
+//
+//  Created by Tommaso Piazza on 29/07/15.
+//  Copyright (c) 2015 Alloc Init. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for stopwatch.
+FOUNDATION_EXPORT double stopwatchVersionNumber;
+
+//! Project version string for stopwatch.
+FOUNDATION_EXPORT const unsigned char stopwatchVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <stopwatch/PublicHeader.h>
+
+
